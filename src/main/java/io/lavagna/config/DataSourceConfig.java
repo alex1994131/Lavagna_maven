1b82ad117c212927b1140ec35e571d9ad7cc652b
Alex1994131 <majesty1994131@outlook.com>
2021-06-02 04:06:47 +0900

    update

--- src/main/java/io/lavagna/config/DataSourceConfig.java
+++ src/main/java/io/lavagna/config/DataSourceConfig.java
@@ -20,6 +20,9 @@ import com.zaxxer.hikari.HikariDataSource;
 import io.lavagna.common.DatabaseMigrationDoneEvent;
 import io.lavagna.common.LavagnaEnvironment;
 import io.lavagna.service.DatabaseMigrator;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.sql.DataSource;
 import org.apache.commons.lang3.ArrayUtils;
 import org.flywaydb.core.api.MigrationVersion;
 import org.hsqldb.util.DatabaseManagerSwing;
@@ -27,93 +30,124 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class DataSourceConfig {
+  public static final MigrationVersion LATEST_STABLE_VERSION = MigrationVersion.fromVersion(
+    "24"
+  );
 
-	public static final MigrationVersion LATEST_STABLE_VERSION = MigrationVersion.fromVersion("24");
+  @Bean
+  public LavagnaEnvironment lavagnaEnvironment(
+    ConfigurableEnvironment environment
+  ) {
+    return new LavagnaEnvironment(environment);
+  }
 
-	@Bean
-    public LavagnaEnvironment lavagnaEnvironment(ConfigurableEnvironment environment) {
-	    return new LavagnaEnvironment(environment);
+  @Bean(destroyMethod = "close")
+  public DataSource getDataSource(LavagnaEnvironment env)
+    throws URISyntaxException {
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setDriverClassName(
+      getDriveClassName(env.getRequiredProperty("datasource.dialect"))
+    );
+
+    if (
+      env.containsProperty("datasource.url") && //
+      env.containsProperty("datasource.username")
+    ) {
+      urlAndCredentials(dataSource, env);
+    } else {
+      urlWithCredentials(dataSource, env);
     }
 
-	// @Bean(destroyMethod = "close")
-	@Bean
-	public DataSource getDataSource(LavagnaEnvironment env) throws URISyntaxException {
-
-		HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName(getDriveClassName(env.getRequiredProperty("datasource.dialect")));
-
-		if (env.containsProperty("datasource.url") && //
-				env.containsProperty("datasource.username")) {
-			urlAndCredentials(dataSource, env);
-		} else {
-			urlWithCredentials(dataSource, env);
-		}
-
-		if (System.getProperty("startDBManager") != null) {
-		    System.setProperty("java.awt.headless", "false");
-			DatabaseManagerSwing.main(new String[] { "--url", "jdbc:hsqldb:mem:lavagna", "--noexit" });
-		}
-
-		return dataSource;
-	}
-
-	// tomcat 8 is picky when we include the drivers in the war
-    private static String getDriveClassName(String dialect) {
-        switch (dialect) {
-            case "HSQLDB":
-                return "org.hsqldb.jdbcDriver";
-            case "MYSQL":
-                return "com.mysql.jdbc.Driver";
-            case "PGSQL":
-                return "org.postgresql.Driver";
-            default:
-                throw new IllegalStateException("Unknown dialect " + dialect);
-        }
+    if (System.getProperty("startDBManager") != null) {
+      System.setProperty("java.awt.headless", "false");
+      DatabaseManagerSwing.main(
+        new String[] { "--url", "jdbc:hsqldb:mem:lavagna", "--noexit" }
+      );
     }
-    //
 
+    return dataSource;
+  }
 
-	/**
-	 * for supporting heroku style url:
-	 *
-	 * <pre>
-	 * [database type]://[username]:[password]@[host]:[port]/[database name]
-	 * </pre>
-	 *
-	 * @param dataSource
-	 * @param env
-	 * @throws URISyntaxException
-	 */
-	private static void urlWithCredentials(HikariDataSource dataSource, LavagnaEnvironment env)
-			throws URISyntaxException {
-		URI dbUri = new URI(env.getRequiredProperty("datasource.url"));
-		dataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
-		dataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
-		dataSource.setJdbcUrl(String.format("%s://%s:%s%s", scheme(dbUri), dbUri.getHost(), dbUri.getPort(), dbUri.getPath()));
-	}
+  // tomcat 8 is picky when we include the drivers in the war
+  private static String getDriveClassName(String dialect) {
+    switch (dialect) {
+      case "HSQLDB":
+        return "org.hsqldb.jdbcDriver";
+      case "MYSQL":
+        return "com.mysql.jdbc.Driver";
+      case "PGSQL":
+        return "org.postgresql.Driver";
+      default:
+        throw new IllegalStateException("Unknown dialect " + dialect);
+    }
+  }
 
-	private static String scheme(URI uri) {
-		return "postgres".equals(uri.getScheme()) ? "jdbc:postgresql" : uri.getScheme();
-	}
+  //
 
-	private static void urlAndCredentials(HikariDataSource dataSource, LavagnaEnvironment env) {
-		dataSource.setJdbcUrl(env.getRequiredProperty("datasource.url"));
-		dataSource.setUsername(env.getRequiredProperty("datasource.username"));
-		dataSource.setPassword(env.getProperty("datasource.password") != null ? env.getProperty("datasource.password") : "");
-	}
+  /**
+   * for supporting heroku style url:
+   *
+   * <pre>
+   * [database type]://[username]:[password]@[host]:[port]/[database name]
+   * </pre>
+   *
+   * @param dataSource
+   * @param env
+   * @throws URISyntaxException
+   */
+  private static void urlWithCredentials(
+    HikariDataSource dataSource,
+    LavagnaEnvironment env
+  )
+    throws URISyntaxException {
+    URI dbUri = new URI(env.getRequiredProperty("datasource.url"));
+    dataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+    dataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
+    dataSource.setJdbcUrl(
+      String.format(
+        "%s://%s:%s%s",
+        scheme(dbUri),
+        dbUri.getHost(),
+        dbUri.getPort(),
+        dbUri.getPath()
+      )
+    );
+  }
 
-	@Bean
-	public DatabaseMigrator migrator(LavagnaEnvironment env, DataSource dataSource, ApplicationEventPublisher publisher) {
+  private static String scheme(URI uri) {
+    return "postgres".equals(uri.getScheme())
+      ? "jdbc:postgresql"
+      : uri.getScheme();
+  }
 
-		boolean isDev = ArrayUtils.contains(env.getActiveProfiles(),"dev");
+  private static void urlAndCredentials(
+    HikariDataSource dataSource,
+    LavagnaEnvironment env
+  ) {
+    dataSource.setJdbcUrl(env.getRequiredProperty("datasource.url"));
+    dataSource.setUsername(env.getRequiredProperty("datasource.username"));
+    dataSource.setPassword(
+      env.getProperty("datasource.password") != null
+        ? env.getProperty("datasource.password")
+        : ""
+    );
+  }
 
-		DatabaseMigrator migrator = new DatabaseMigrator(env, dataSource, isDev ? MigrationVersion.LATEST : LATEST_STABLE_VERSION);
-		publisher.publishEvent(new DatabaseMigrationDoneEvent(this));
-		return migrator;
-	}
+  @Bean
+  public DatabaseMigrator migrator(
+    LavagnaEnvironment env,
+    DataSource dataSource,
+    ApplicationEventPublisher publisher
+  ) {
+    boolean isDev = ArrayUtils.contains(env.getActiveProfiles(), "dev");
+
+    DatabaseMigrator migrator = new DatabaseMigrator(
+      env,
+      dataSource,
+      isDev ? MigrationVersion.LATEST : LATEST_STABLE_VERSION
+    );
+    publisher.publishEvent(new DatabaseMigrationDoneEvent(this));
+    return migrator;
+  }
 }
